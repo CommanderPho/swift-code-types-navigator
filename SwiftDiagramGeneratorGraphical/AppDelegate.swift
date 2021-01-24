6b66416c925707a2c54c48229b0d591e2ef58f5e
CommanderPho <PhoHale@gmail.com>
2021-01-24 09:21:38 -0500

    Added macOS menuBar icons

--- SwiftDiagramGeneratorGraphical/AppDelegate.swift
+++ SwiftDiagramGeneratorGraphical/AppDelegate.swift
@@ -10,7 +10,26 @@ import SwiftDiagramComponentsGenerator
 
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, MacOSMenuBarExtraProviderProtocol {
+
+	// applicationStatus: should be updated when loading is complete to indicate that it's ready to use.
+	var applicationStatus: ApplicationStatus = .Normal {
+		didSet {
+			print("applicationStatus changed: \(self.applicationStatus)")
+			self.updateMenuBarExtra()
+		}
+	}
+
+
+	
+	////////////////////////////////////////////////////////////////////
+	//MARK: -
+	//MARK: - MacOSMenuBarExtraProviderProtocol properties
+	var menuBarExtraStatusItem: NSStatusItem!
+	// The Menu that contains the menuBar Item, displayed in the right side of the macOS MenuBar. (Not the app menu)
+	@IBOutlet weak var menuBarExtraMainMenu: NSMenu!
+
+
 
 //	var visualizationDirectoryPathString: URL = URL(fileURLWithPath: "/Volumes/Speakhard/Repo/swift-code-types-navigator", isDirectory: true)
 
@@ -23,6 +42,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
+		// Setup the persistant menuBar status Item on the right side of the macOS menubar
+		self.setupMenuBarExtra() // I don't need to explicitly call setup since it's called in updateMenuBarExtra() if it isn't already done, but I do anyway.
+		self.updateMenuBarExtra()
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
@@ -73,3 +95,47 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 }
 
+
+
+extension AppDelegate {
+
+	////////////////////////////////////////////////////////////////////
+	//MARK: -
+	//MARK: - MacOSMenuBarExtraProviderProtocol: macOS MenuBar Status Item (the agent in the right side of the menubar)
+
+
+	func updateMenuBarExtra() {
+		let menuBarExtra: NSStatusItem
+		if let validMenuBarExtra = self.menuBarExtraStatusItem {
+			menuBarExtra = validMenuBarExtra
+		}
+		else {
+			// Initialize a new one
+			self.setupMenuBarExtra()
+			guard let validMenuBarExtra = self.menuBarExtraStatusItem else {
+				fatalError("No valid menu status bar extra exists even though setupMenuBarExtra() was called!")
+			}
+			menuBarExtra = validMenuBarExtra
+		}
+
+		// Apply the status state to the menuBarExtra, updating the text and image if needed
+		self.applicationStatus.menuBarStatus.apply(menuBarExtra)
+	}
+
+	// called only once at launch
+	func setupMenuBarExtra() {
+		// Make a status bar that has variable length (as opposed to being a standard square size)
+		// -1 to indicate "variable length"
+		self.menuBarExtraStatusItem = NSStatusBar.system.statusItem(withLength: -1)
+
+		// Set the menu that should appear when the item is clicked
+		self.menuBarExtraStatusItem!.menu = self.menuBarExtraMainMenu
+
+		// Set if the item should change color when clicked
+		self.menuBarExtraStatusItem!.highlightMode = true
+	}
+
+
+
+
+}
